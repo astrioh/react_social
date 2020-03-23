fc20abf1cc4032e6ad32b17aa8de33776993644f
AstRioh <korova674@gmail.com>
2020-03-24 01:32:47 +0800

    small class change

--- src/blocks/Header/Header.jsx
+++ src/blocks/Header/Header.jsx
@@ -4,7 +4,7 @@ import s from './Header.module.css';
 const Header = () => {
     return (
         <header className={s.header}>
-            <img src="https://cdn.shopify.com/shopifycloud/hatchful-web/assets/6fcc76cfd1c59f44d43a485167fb3139.png" alt="Logo" />
+            <img className={s.header__logo} src="https://cdn.shopify.com/shopifycloud/hatchful-web/assets/6fcc76cfd1c59f44d43a485167fb3139.png" alt="Logo" />
         </header>
     );
 }
