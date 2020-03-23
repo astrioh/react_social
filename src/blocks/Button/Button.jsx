2a10bdef764ce446d748cf93c9ce1f35efa22e3c
AstRioh <korova674@gmail.com>
2020-03-24 01:32:57 +0800

    button layout

---
+++ src/blocks/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Button.module.css';
+
+const Button = (props) => {
+    return (
+        <button className={s.button}>{props.text}</button>
+    );
+}
+
+export default Button;
