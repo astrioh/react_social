75cf44252fc3f8917285e81d57be97fef63b8815
AstRioh <korova674@gmail.com>
2020-03-27 10:48:44 +0800

    add routing

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import "./App.css";
 import Header from "./blocks/Header/Header";
 import Navbar from "./blocks/Navbar/Navbar";
 import Profile from "./blocks/Profile/Profile";
+import Messages from "./blocks/Messages/Messages";
+import News from "./blocks/News/News";
+import Music from "./blocks/Music/Music";
+import Settings from "./blocks/Settings/Settings";
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile name="Ilya Butskikh" dateOfBirth="19.02.2001" city="Yaroslavl'" education="YSTU" contacts="+79159732069" />
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        {/*<Profile name="Ilya Butskikh" dateOfBirth="19.02.2001" city="Yaroslavl'" education="YSTU" contacts="+79159732069" />*/}
+        <div className="content">
+          <Route path="/profile" component={Profile} />
+          <Route path="/messages" component={Messages} />
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/blocks/Navbar/Navbar.jsx
+++ src/blocks/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import s from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className={s.navbar}>
             <ul className={s.navbar__menu}>
-                <li className={s.navbar__listItem}><a className={s.navbar__link} href="#mockup_change-me-later">Profile</a></li>
-                <li className={s.navbar__listItem}><a className={s.navbar__link} href="#mockup_change-me-later">Messages</a></li>
-                <li className={s.navbar__listItem}><a className={s.navbar__link} href="#mockup_change-me-later">News</a></li>
-                <li className={s.navbar__listItem}><a className={s.navbar__link} href="#mockup_change-me-later">Music</a></li>
-                <li className={s.navbar__listItem}><a className={s.navbar__link} href="#mockup_change-me-later">Settings</a></li>
+                <li className={s.navbar__listItem}><NavLink activeClassName={s.active} to='/profile' className={s.navbar__link}>Profile</NavLink></li>
+                <li className={s.navbar__listItem}><NavLink activeClassName={s.active} to='/messages' className={s.navbar__link}>Messages</NavLink></li>
+                <li className={s.navbar__listItem}><NavLink activeClassName={s.active} to='/news' className={s.navbar__link}>News</NavLink></li>
+                <li className={s.navbar__listItem}><NavLink activeClassName={s.active} to='/music' className={s.navbar__link}>Music</NavLink></li>
+                <li className={s.navbar__listItem}><NavLink activeClassName={s.active} to='/settings' className={s.navbar__link}>Settings</NavLink></li>
             </ul>
         </nav>
     );
