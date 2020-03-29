10b1d297be0c2e403c8b909531fab2997bc68312
AstRioh <korova674@gmail.com>
2020-03-29 20:46:37 +0800

    data refactor

--- src/blocks/Profile/Profile.jsx
+++ src/blocks/Profile/Profile.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = (props) => {
     return (
         <div className={s.profile}>
-            <img
-                className={s.profile__bgImage}
-                src="https://openimagedenoise.github.io/images/moana_16spp_oidn.jpg"
-                alt="main bg"
+            <ProfileInfo 
+                profileData={props.profileData}
+                id={props.id}
             />
-            <img className={s.profile__avatar} src="https://sun9-52.userapi.com/c852228/v852228024/8cdcb/X08CH8nHDN4.jpg" alt="Profile pic"/>
-            <div className={s.profile__info}>
-                <div className={s.profile__name}>{props.name}</div>
-                <div>Date of Birth: {props.dateOfBirth}</div>
-                <div>City: {props.city}</div>
-                <div>Education: {props.education}</div>
-                <div>Contacts: {props.contacts}</div>
-            </div>
-            <MyPosts />
+            <MyPosts postData={props.postData} />
         </div>
     )
 }
