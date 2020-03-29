39c163a58196e3d9a7d924c15eee824a76844108
AstRioh <korova674@gmail.com>
2020-03-29 20:47:58 +0800

    add likecount to post component

--- src/blocks/Profile/MyPosts/Post/Post.jsx
+++ src/blocks/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import s from './Post.module.css';
+import LikeCount from '../../../LikeCount/LikeCount';
 
 
 
 const Post = (props) => {
     return (
-        <div className={s.post}>{props.text}</div>
+        <div className={s.post}>
+            <span className={s.post__text}>{props.text}</span>
+            <LikeCount count={props.likes} />
+        </div>
     );
 }
 
